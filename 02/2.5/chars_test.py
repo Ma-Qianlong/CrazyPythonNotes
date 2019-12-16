b26fdebcc6f255e83ccfb8bcbaa2dadebbc85518
maqianlong <maql@deepctrls.com>
2019-12-16 22:55:50 +0800

    chars test part 1

---
+++ 02/2.5/chars_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# -*- *************** -*-
+# @File  : chars_test.py
+# @Description : 字符串 序列相关
+# @Author: mql
+# @Time  : 2019/12/13 15:28
+# -*- *************** -*-
+
+
+s = 'crazyit.org is very good'
+# 获取字符串中索引为2的字符
+print(s[2])
+
+# 获取从右往左，第4个字符
+print(s[-4])

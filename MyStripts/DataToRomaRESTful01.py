efc8c94a7d17d856578e20e38861fd2d79f8bba6
maqianlong <1430753921@qq.com>
2020-06-22 17:42:31 +0800

    some update for MyStripts

---
+++ MyStripts/DataToRomaRESTful01.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# -*- *************** -*-
+# @File  : DataToRomaRESTful.py
+# @Description :
+# @Author: mql
+# @Time  : 2020/5/13 16:37
+# -*- *************** -*-
+
+
+from flask import Flask
+from flask_restful import Api, reqparse, abort, Resource, request
+from flask_cors import *
+from RandomDataToRoma01 import manyTagDataToMySQL2 as d2db
+import time
+import re
+
+p=re.compile(r'[-,$()#+&*]')
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)  # 设置跨域
+api = Api(app)
+
+def abort_if_param_doesnt_exist(args):
+    if False:
+        abort(404, message="the requirted param is empty")
+
+
+# parser = reqparse.RequestParser()
+# parser.add_argument("tagnames")
+
+# parser = reqparse.RequestParser
+# parser.add_argument()
+# parser.add_argument("tagnames")
+# parser.add_argument('sT')
+# parser.add_argument('eT')
+# parser.add_argument('min')
+# parser.add_argument('max')
+
+
+class DoDataToMySQL(Resource):
+    def post(self):
+        # args = parser.parse_args()
+        args = request.json
+        print("请求了向MySQL历史数据库模拟数据：" + str(args))
+        tagnames = str(args["tagnames"]).replace("#", "_")
+        # tagnames = re.sub(re.compile(r'[#]'), "_", args["tagnames"])
+        print(tagnames)
+        # print(re.sub(re.compile(r'[#]'), '_', "test#test"))
+        # print("test#test".replace("#", "_"))
+        try:
+            d2db(tagnames=tagnames, sTime=args["sT"], eTime=args["eT"],
+                 minVal=float(args["min"]) if 'min' in args else 0,
+                 maxVal=float(args["max"]) if 'max' in args else 255,
+                 isFixedVal=args["isFixedVal"] if 'isFixedVal' in args else 'no',
+                 host=args["host"] if 'host' in args else '192.168.5.249',
+                 port=int(args["port"]) if 'port' in args else 3136,
+                 user=args["user"] if 'user' in args else "root",
+                 password=args["password"] if 'password' in args else "root",
+                 database=args["database"] if 'database' in args else 'db-deepctrls-cecep-roma')
+            print("当前进程使用CPU时间(ms): %f" % (time.process_time_ns() / 1000000))
+        except Exception as e:
+            print(type(e.args))
+            print(e.args)
+            return {'success': False, 'msg': e.args[0]}, 201
+        else:
+            return {'success': True, 'msg': '模拟MySQL历史数据完成！'}, 201
+
+
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(DoDataToMySQL, '/data2mysql')
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ MyStripts/RandomDataToRoma01.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+
+# -*- *************** -*-
+# @File  : RandomDataToRoma.py
+# @Description : 向MySQL数据库模拟随机数据
+# @Author: mql
+# @Time  : 2020/5/12 17:47
+# -*- *************** -*-
+
+
+from datetime import datetime, timedelta
+import random
+import pymysql
+import time
+
+
+# 获取俩浮点数之间的伪随机数
+def getRandomFloat(sF, eF):
+    randomStr = '%.2f' % random.uniform(sF, eF)
+    return float(randomStr)
+
+
+# 获取每几分钟数据
+def getEveryMinute(begin_time, end_time, durion):
+    '''
+    获取每几分钟数据
+    :param begin_time: string 开始时间
+    :param end_time: string 结束时间
+    :param durion: integer 分钟间隔
+    :return: list 时间字符串列表
+    '''
+    time_list = []
+    begin_time = datetime.strptime(begin_time, '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+    while begin_time <= end_time:
+        time_str = begin_time.strftime('%Y-%m-%d %H:%M:%S')
+        time_list.append(time_str)
+        begin_time += timedelta(minutes=durion)
+    return time_list
+
+
+# 向数据库写数据，datas元组
+def exec_many(datas, host='192.168.5.249', port=3136, user="root", password="root",
+              database='db-deepctrls-cecep-roma'):
+    conn = pymysql.connect(host=host, port=port, user=user, password=password,
+                           database=database)
+    cursor = conn.cursor()
+    try:
+        affRows = cursor.executemany('insert into cecep_czxl_hvc_opc_upload (id, dat, value) values (%s, %s, %s)', datas)
+        print("【insert】 Number of affected rows: %d" % affRows)
+        conn.commit()
+    except Exception as e:
+        print("execute many error, args: %s" % e.args)
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def exec_del(tagnameList, sTime, eTime, host='192.168.5.249', port=3136, user="root", password="root",
+             database='db-deepctrls-cecep-roma'):
+    conn = pymysql.connect(host=host, port=port, user=user, password=password,
+                           database=database)
+    cursor = conn.cursor()
+    try:
+        argList = [];
+        for tagname in tagnameList:
+            # sql = 'delete from cecep_huawei_roma_test where id = \'%s\' and time between \'%s\' and \'%s\'' % ('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59')
+            # print(sql)
+            # affRows = cursor.execute(sql)
+            # affRows = cursor.execute("delete from cecep_huawei_roma_test where id = 'test_test_test_test_test' and time between '2020-05-10 00:00:00' and '2020-05-13 23:59:59'")
+            # affRows = cursor.execute("delete from cecep_huawei_roma_test where id = %s and time between %s and %s ", ('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59'))
+            # affRows = cursor.executemany("delete from cecep_huawei_roma_test where id = %s and time between %s and %s ", [('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59')])
+            # print("Number of affected rows: %d" % affRows)
+            argList.append((tagname, sTime, eTime))
+        affRows = cursor.executemany("delete from cecep_czxl_hvc_opc_upload where id = %s and dat between %s and %s ",
+                                     argList)
+        print("【delete】 Number of affected rows: %d" % affRows)
+        conn.commit()
+    except Exception as e:
+        print("execute many Delete error, args: %s" % e.args)
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# 模拟单个tagname的值到MySQL数据库
+def oneTagDataToMySQL(tagname, sT, eT):
+    timeList = getEveryMinute(sT, eT, 5)
+    lastTupleList = []
+    for time in timeList:
+        tupleD = (tagname, time, getRandomFloat(0, 255))
+        lastTupleList.append(tupleD)
+    print('data list for 【%s】 size: %d' % (tagname, len(lastTupleList)))
+    exec_many(tuple(lastTupleList))
+
+
+# 模拟多个tagname的值到MySQL数据库
+def manyTagDataToMySQL(tagnames, sT, eT, minVal=0, maxVal=255):
+    sRT = datetime.now()
+    print(sRT.timestamp())
+    if tagnames == None or len(tagnames) < 1:
+        raise ValueError('tagname 不能为空，多个用英文逗号分隔')
+    tagList = tagnames.split(",")
+
+    # 先删除
+    print(">>>>>1. 删除已有的满足当前条件的数据，防冲突")
+    exec_del(tagList, sTime, eTime)
+
+    timeList = getEveryMinute(sT, eT, 5)
+    toSubmitList = []
+    count = 0
+    print(">>>>>2. 开始组织数据写入。。。")
+    for tag in tagList:
+        for time in timeList:
+            tupleD = (tag, time, getRandomFloat(minVal, maxVal))
+            tupleDel = (tag, time)
+
+            toSubmitList.append(tupleD)
+            count += 1
+            if count % 10000 == 0:
+                exec_many(tuple(toSubmitList))
+                toSubmitList.clear()
+
+    if len(toSubmitList) > 0:
+        exec_many(tuple(toSubmitList))
+    print(datetime.now().timestamp())
+    print(">>>>>3. 模拟随机数据写入完成, 共写入 %d 条数据, 模拟及写入耗时(s)：%f" % (count, datetime.now().timestamp() - sRT.timestamp()))
+
+
+# 模拟多个tagname的值到指定的 MySQL 数据库
+def manyTagDataToMySQL2(tagnames, sTime, eTime, minVal=0, maxVal=255,
+                        isFixedVal = 'no',
+                        host='192.168.5.249', port=3136, user="root",
+                        password="root",
+                        database='db-deepctrls-cecep-roma'):
+    '''
+    :param tagnames:
+    :param sTime:
+    :param eTime:
+    :param minVal:
+    :param maxVal:
+    :param isFixedVal: 是否固定值，yes-是（取maxVal为固定值）；no-否(默认, 取伪随机)
+    :param host:
+    :param port:
+    :param user:
+    :param password:
+    :param database:
+    :return:
+    '''
+    sRT = datetime.now()
+    print(sRT.timestamp())
+    if tagnames == None or len(tagnames) < 1:
+        raise ValueError('tagname 不能为空，多个用英文逗号分隔')
+    tagList = tagnames.split(",")
+
+    # 先删除
+    print(">>>>>1. 删除已有的满足当前条件的数据，防冲突")
+    exec_del(tagList, sTime, eTime, host=host, port=port, user=user, password=password, database=database)
+
+    timeList = getEveryMinute(sTime, eTime, 5)
+    toSubmitList = []
+    count = 0
+    print(">>>>>2. 开始组织数据写入。。。")
+    for tag in tagList:
+        for time in timeList:
+            tupleD = (tag, time, maxVal)
+
+            if isFixedVal == 'no':
+                tupleD = (tag, time, getRandomFloat(minVal, maxVal))
+
+            toSubmitList.append(tupleD)
+            count += 1
+            if count % 10000 == 0:
+                exec_many(tuple(toSubmitList), host=host, port=port, user=user, password=password, database=database)
+                toSubmitList.clear()
+
+    if len(toSubmitList) > 0:
+        exec_many(tuple(toSubmitList), host=host, port=port, user=user, password=password, database=database)
+    print(datetime.now().timestamp())
+    print(">>>>>3. 模拟随机数据写入完成, 共写入 %d 条数据, 模拟及写入耗时(s)：%f" % (count, datetime.now().timestamp() - sRT.timestamp()))
+
+
+# oneTagDataToMySQL("test_test_test_test_test", '2020-05-10 00:00:00', '2020-05-10 23:00:00')
+# exec_del(['test_test_test_test_test'], '2020-05-10 00:00:00', '2020-05-13 23:59:59')
+
+if __name__ == "__main__":
+    print('------------------欢迎使用MySQL历史数据模拟(5分钟间隔)-------------------')
+    print('注：此处会将相同tagname及时间的值覆盖', end='\n\n')
+    tagnames = input("请输入tagname, 多个应英文逗号（','）分隔：")
+    sDate = input("请输入开始日期(yyyy-MM-dd)：")
+    eDate = input("请输入结束日期(yyyy-MM-dd)：")
+
+    sTime = sDate + " 00:00:00"
+    eTime = eDate + " 23:59:59"
+
+    manyTagDataToMySQL(tagnames, sTime, eTime)
+
+    print("当前进程使用CPU时间(ms): %f" % (time.process_time_ns() / 1000000))
+
+# conn = pymysql.connect(host='192.168.5.249', port=3136, user="root", password="root",
+#                            database='db-deepctrls-cecep-roma')
+# cursor = conn.cursor()
+# sql = 'delete from cecep_huawei_roma_test where id = \'%s\' and time between \'%s\' and \'%s\'' % ('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59')
+# print(sql)
+# # affRows = cursor.execute(sql)
+# # affRows = cursor.execute("delete from cecep_huawei_roma_test where id = 'test_test_test_test_test' and time between '2020-05-10 00:00:00' and '2020-05-13 23:59:59'")
+# # affRows = cursor.execute("delete from cecep_huawei_roma_test where id = %s and time between %s and %s ", ('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59'))
+# affRows = cursor.executemany("delete from cecep_huawei_roma_test where id = %s and time between %s and %s ", [('test_test_test_test_test', '2020-05-10 00:00:00', '2020-05-13 23:59:59')])
+# print("Number of affected rows: %d" % affRows)
+# conn.commit()
+# cursor.close()
+# conn.close()

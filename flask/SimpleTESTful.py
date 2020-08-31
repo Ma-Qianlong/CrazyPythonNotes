b87e4738b513d4cb976a9ef65f04e911ad88e13c
maqianlong <1430753921@qq.com>
2020-08-31 10:15:44 +0800

    test for something

--- flask/SimpleTESTful.py
+++ flask/SimpleTESTful.py
@@ -60,6 +60,23 @@ def hello_world():
     print("=========成功 生成 index.html================")
     return jsonify(args=args, form=form)
 
+@app.route("/test/sd/data", methods=['GET', 'POST'])
+def testSDdata():
+    print(request.args)
+    return '''{"apiVersion":"1.0.0","code":"success","message":"","requestId":"ID-daas-0-daas-headless-anyonedev-svc-cluster-local-37795-1597647990788-0-573",
+                "result":[{"PROJECTUID":"（资源中心）车站商铺用电","RESULTCOUNT":43757.7},{"PROJECTUID":"（资源中心）中国电信","RESULTCOUNT":26296.4},{"PROJECTUID":"总电量","RESULTCOUNT":21792457.01},
+                {"PROJECTUID":"(中铁二局工程有限公司)中山八站","RESULTCOUNT":187.2},{"PROJECTUID":"（资源中心）中国移动","RESULTCOUNT":49028.72},
+                {"PROJECTUID":"（资源中心）自助设备用电","RESULTCOUNT":4323.36},{"PROJECTUID":"非运营总部办公用电","RESULTCOUNT":34565},
+                {"PROJECTUID":"资源设备用电","RESULTCOUNT":381551.82},{"PROJECTUID":"（后勤中心）用电","RESULTCOUNT":25412},
+                {"PROJECTUID":"（房产事业总部）单车衔接用电","RESULTCOUNT":0},{"PROJECTUID":"外单位借用电","RESULTCOUNT":320705.84},
+                {"PROJECTUID":"（资源中心）PIDS用电","RESULTCOUNT":70333.12},{"PROJECTUID":"（资源中心）广告用电","RESULTCOUNT":167881.96},
+                {"PROJECTUID":"照明用电","RESULTCOUNT":673939.46},{"PROJECTUID":"（房产事业总部）临时公交站场用电","RESULTCOUNT":917.28},
+                {"PROJECTUID":"（中国铁塔股份有限公司）鱼珠车辆段","RESULTCOUNT":317768.88},{"PROJECTUID":"环控用电","RESULTCOUNT":7718358.6},
+                {"PROJECTUID":"运营用电","RESULTCOUNT":21055634.35},{"PROJECTUID":"办公用电","RESULTCOUNT":5867.86},
+                {"PROJECTUID":"牵引用电","RESULTCOUNT":12657468.43},{"PROJECTUID":"（环境公司）用电","RESULTCOUNT":3},
+                {"PROJECTUID":"（房产事业总部）机动车停车场用电","RESULTCOUNT":870.48},{"PROJECTUID":"（中国邮政集团公司广州市分公司）区庄站","RESULTCOUNT":962},
+                {"PROJECTUID":"（广州市荔湾区南源街市容环境卫生站）西场站","RESULTCOUNT":0},{"PROJECTUID":"（资源中心）中国联通","RESULTCOUNT":19930.56},
+                {"PROJECTUID":"（广州市公交公安分局）用电","RESULTCOUNT":9150}]}'''
 
 if __name__ == "__main__":
     # 将host设置为0.0.0.0，则外网用户也可以访问到这个服务

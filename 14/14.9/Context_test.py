2393979b282b7d215dd29c2cf8dd9f7b2fe62b48
maqianlong <1430753921@qq.com>
2020-09-29 17:45:27 +0800

    about process with set_start_method and Context to ctreate process

---
+++ 14/14.9/Context_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# -*- *************** -*-
+# @File  : Context_test.py
+# @Description : 
+# @Author: mql
+# @Time  : 2020/9/29 16:08
+# -*- *************** -*-
+
+
---
+++ 14/14.9/start_method_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# -*- *************** -*-
+# @File  : start_method_test.py
+# @Description : 
+# @Author: mql
+# @Time  : 2020/9/29 15:57
+# -*- *************** -*-
+
+

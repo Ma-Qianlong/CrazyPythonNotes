ef6d2f2477afa0fa378c32ebdec4281fa6f8440d
maqianlong <1430753921@qq.com>
2020-09-29 17:46:19 +0800

    upload last files

--- 14/14.9/Context_test.py
+++ 14/14.9/Context_test.py
@@ -8,3 +8,24 @@
 # -*- *************** -*-
 
 
+import multiprocessing
+import os
+
+def foo(q):
+    print('被启动的新线程：（%s）' % os.getpid())
+    q.put('Python')
+
+
+if __name__ == "__main__":
+    # 设置使用fork方式启动进程, 并获取Context对象
+    # ctx = multiprocessing.get_context('fork')  # 只能UNIX平台
+    ctx = multiprocessing.get_context('spawn')  # 只能Windows平台
+    # 接下来就可以使用 Context 对象来代替 mutliprocessing 模块
+    q = ctx.Queue()
+    # 创建进程
+    mp = ctx.Process(target=foo, args=(q,))
+    # 启动进程
+    mp.start()
+    # 获取队列中的消息
+    print(q.get())
+    mp.join()
--- 14/14.9/start_method_test.py
+++ 14/14.9/start_method_test.py
@@ -8,3 +8,24 @@
 # -*- *************** -*-
 
 
+import multiprocessing
+import os
+
+
+def foo(q):
+    print('被启动的新线程：（%s）' % os.getpid())
+    q.put('Python')
+
+
+if __name__ == "__main__":
+    # 设置使用fork方式启动进程
+    # multiprocessing.set_start_method('fork')  # 只能UNIX平台
+    multiprocessing.set_start_method('spawn')  # 只能Windows平台
+    q = multiprocessing.Queue()
+    # 创建进程
+    mp = multiprocessing.Process(target=foo, args=(q,))
+    # 启动进程
+    mp.start()
+    # 获取队列中的消息
+    print((q.get()))
+    mp.join()
